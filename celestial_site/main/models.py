32af9633fc54fb82f13a277784d8eb1bbb6db4b1
Igor <martynov.igor2020ty@gmail.com>
2024-12-05 15:52:22 +0000

    feat(main app): add models, urls, and views
    - Introduced Category and Celestial models to represent different categories and celestial objects.
      - Created Category model with name and slug fields.
      - Created Celestial model with name, category, description, etc.  fields.
    - Implemented URL configurations for the main application.
      - Added URL patterns for index, celestial list, and celestial detail pages.
    - Developed views for the main application.
      - Created index view for displaying the homepage.
      - Created celestial_list view for displaying a list of celestial objects.
      - Created detail view for displaying individual celestial objects with their respective details.
    
    This commit sets up the core functionality of the main application, enabling the creation and management of celestial objects and their respective categories.

---
+++ celestial_site/main/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Celestial(models.Model):
+    category = models.ForeignKey('Category', related_name='celestial', on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    discount = models.DecimalField(max_digits=4, default=0.00, decimal_places=2)
+    created =  models.TimeField(auto_now_add=True)
+    updated = models.TimeField(auto_now=False)
+    image = models.ImageField(upload_to='celestial/%Y/%m/%d', blank=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['id', 'slug']), 
+                   models.Index(fields=['name'])]
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("main:celestial_list", kwargs={"celestial_slug": self.slug})
+    
+    def cell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+        return self.price
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    slug = models.SlugField(max_length=25, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['slug'])]
+        
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("main:celestial_list", kwargs={"category_slug": self.slug})
+    
---
+++ celestial_site/main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Celestial, Category
+
+
+def index(request):
+    celestial_objs = Celestial.objects.filter(available=True)
+    return render(request, 'main/index.html', context={'celestial_objs': celestial_objs})
+
+def detail(request, celestial_slug):
+    celestial_obj = get_object_or_404(Celestial, slug=celestial_slug)
+    return render(request, 'main/detail.html', context={'celestial_obj': celestial_obj})
+
+def celestial_list(request, category_slug=None):
+    category = None
+    celestial_objs = Celestial.objects.filter(available=True)
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        celestial_objs = category.celestial.filter(available=True)
+    return render(request, 'main/celestial_list.html', context={'celestial_objs': celestial_objs,
+                                                                'category': category,
+                                                                'categories': categories})
